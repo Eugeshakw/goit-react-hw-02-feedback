07266cee66951499456beac8255e93e10320b117
Eugeshakw <eugeshakw@gmail.com>
2023-07-20 15:39:45 +0300

    update

--- src/components/Statistics/statistics.jsx
+++ src/components/Statistics/statistics.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-
+import style from './statistics.module.scss';
 const Statistics = ({good, neutral, bad, total, countPercentage}) => {
     return (
-        <div>
-            <p>Good: {good} </p>
-            <p>Neutral: {neutral} </p>
-            <p>Bad: {bad}</p>
-            <p>Total:{total}</p>
-            <p>Positive feedback: {countPercentage.toFixed(0)}%</p>
+        <div className={style.confeedback}>
+            <p className={style.statfeedback}>Good: {good} </p>
+            <p className={style.statfeedback}>Neutral: {neutral} </p>
+            <p className={style.statfeedback}>Bad: {bad}</p>
+            <p className={style.statfeedback}>Total:{total}</p>
+            <p className={style.statfeedback}>Positive feedback: {countPercentage.toFixed(0)}%</p>
         </div>
     )
 }
--- src/components/feedbackOptions/feedbackOptions.jsx
+++ src/components/feedbackOptions/feedbackOptions.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { feedbacks } from 'utils/utils';
 import { nanoid } from 'nanoid';
+import style from './feedbackOption.module.scss'
 const FeedBackOptions = ({ onLeaveFeedback }) => {
   return (
-    <div>
+    <div className={style.containerbtn}>
       {Object.keys(feedbacks).map(feedback => (
         <button
+          className={style.btnfeedback}
           type="button"
           key={nanoid()}
           onClick={() => onLeaveFeedback(feedback)}
